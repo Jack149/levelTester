4c729752c42a65d4eb06577d0701f7b8aa87bc52
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-13 11:19:39 -0600

    Started to Add radio support

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-let x = 0
 let y = 0
+let x = 0
+radio.setGroup(1)
+radio.setTransmitPower(7)
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
