dbd0ec87b295925e8e6bffcca59eaab1f275c723
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-19 00:03:28 -0600

    made changes to wrong code, trying to reverse it.

--- main.ts
+++ main.ts
@@ -1,17 +1,16 @@
-let TILT = 0
 let y = 0
 let x = 0
-let OLD_TILT = 0
+let TILT = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
-    if (Math.abs(x) > 64) {
+    if (Math.abs(x) > 128) {
         basic.showIcon(IconNames.No)
         radio.sendNumber(111)
         TILT += 1
-    } else if (Math.abs(y) > 64) {
+    } else if (Math.abs(y) > 128) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -24,6 +23,5 @@ basic.forever(function () {
     } else {
         basic.showIcon(IconNames.Yes)
         radio.sendNumber(0)
-        TILT += 1
     }
 })
