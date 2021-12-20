e965d595dc42e3f2960a1e14fd64a2ff67bbafac
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-19 18:18:51 -0600

    Added a tilt counter

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(TILT)
+    basic.pause(100)
+    basic.clearScreen()
+})
 let y = 0
 let x = 0
 let TILT = 0
+TILT = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
-    if (Math.abs(x) > 192) {
+    if (Math.abs(x) > 256) {
         radio.sendNumber(111)
         TILT += 1
-    } else if (Math.abs(y) > 192) {
+    } else if (Math.abs(y) > 256) {
         radio.sendNumber(111)
         TILT += 1
     } else {
