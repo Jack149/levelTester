8af5b30b35f6dcc0060d55c1363ad2c4b8609154
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-12-19 20:24:41 -0600

    Compleltly lost it.

--- main.ts
+++ main.ts
@@ -1,24 +1,22 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(TILT)
-    basic.pause(100)
+    basic.pause(2000)
     basic.clearScreen()
 })
-let y = 0
-let x = 0
 let TILT = 0
-TILT = 0
+let x = input.acceleration(Dimension.X)
+let y = input.acceleration(Dimension.Y)
 radio.setGroup(1)
 radio.setTransmitPower(7)
-basic.forever(function () {
-    x = input.acceleration(Dimension.X)
-    y = input.acceleration(Dimension.Y)
-    if (Math.abs(x) > 256) {
-        radio.sendNumber(111)
-        TILT += 1
-    } else if (Math.abs(y) > 256) {
-        radio.sendNumber(111)
-        TILT += 1
-    } else {
-        radio.sendNumber(0)
-    }
-})
+if (Math.abs(x) > 256) {
+    radio.sendNumber(111)
+    TILT += 1
+} else if (Math.abs(y) > 256) {
+    radio.sendNumber(111)
+    TILT += 1
+} else {
+    radio.sendNumber(0)
+}
